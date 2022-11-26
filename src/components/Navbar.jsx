85b7a26edde5c9f9c1de4e419107475d2012c765
Javier Sintes <javisin98@gmail.com>
2022-11-26 22:44:08 +0000

    style: add logo on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import '../scss/navbar.scss';
+import logo from '../assets/logo.png';
 
-function Navbar() {
+function Navbar({ handleConnectWallet, currentAccount }) {
   return (
     <nav className="navbar navbar-dark bg-black text-white">
-      <span className="navbar-brand mb-0 h1">Akuma NFTs</span>
+      <div className="col-4" />
+      <div className="col-4 text-center">
+        <img className="navbar-brand mb-0 text-center" src={logo} width="150px" alt="Akuma Logo" />
+      </div>
+      <div className="col-4 text-end">
+        {currentAccount
+          ? (
+            <button type="button" disabled onClick={handleConnectWallet} className="btn btn-dark mt-0 p-2 navbar-text text-end me-3 ">
+              {currentAccount.slice(0, 8)}
+              ...
+            </button>
+          )
+          : (
+            <button type="button" onClick={handleConnectWallet} className="btn btn-dark mt-0 p-2 navbar-text text-end me-3 ">
+              Connect Wallet
+            </button>
+          )}
+      </div>
     </nav>
   );
 }
